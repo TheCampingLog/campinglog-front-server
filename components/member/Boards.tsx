60d63187edf20b67607215b3c49064a5ebb26d5d
goldtwo1989 <ttpd19891213@gmail.com>
2025-08-23 04:12:04 +0900

    refactor: 이미지 없는 게시글 미리보기 숨김처리 / 게시글 리스트 디자인

--- components/member/Boards.tsx
+++ components/member/Boards.tsx
@@ -29,16 +29,18 @@ export default function Boards() {
       <h2 className="text-xl font-bold mb-6">- 내 게시글 조회</h2>
 
       {/* 게시글 리스트 */}
-      <ul className="space-y-4 flex-1">
+      <ul className="space-y-5 basis-2/3">
         {boards.map((b) => (
           <li key={b.boardId}>
             <Link
               href={`/boards/${b.boardId}`}
-              className="block bg-[#FFF9E6] rounded-xl p-4 flex gap-4 hover:shadow-md hover:bg-[#fff3cc] transition"
+              className="block bg-[#FFF9E6] rounded-xl p-4 flex gap-4 
+              hover:shadow-md hover:bg-[#fff3cc] transition cursor-pointer 
+              min-h-36 sm:min-h-36 lg:min-h-36"
             >
-            {b.boardImage ? (
+            {/* {b.boardImage ? (
               <img
-                src={b.boardImage}
+                src={`${process.env.NEXT_PUBLIC_IMAGE_ROOT_URL}${b.boardImage}`}
                 alt={b.title}
                 className="w-28 h-28 object-cover rounded-lg"
               />
@@ -46,6 +48,14 @@ export default function Boards() {
               <div className="w-28 h-28 bg-gray-100 flex items-center justify-center text-sm text-gray-400 rounded-lg">
                 No Image
               </div>
+            )} */}
+                {/* ✅ 이미지 있을 때만 표시 */}
+            {b.boardImage && (
+              <img
+                src={`${process.env.NEXT_PUBLIC_IMAGE_ROOT_URL}${b.boardImage}`}
+                alt={b.title}
+                className="w-28 h-28 object-cover rounded-lg"
+              />
             )}
             <div className="flex-1">
               <h3 className="font-bold text-2xl">{b.title}</h3>
@@ -57,7 +67,7 @@ export default function Boards() {
         ))}
       </ul>
     {/* ✅ 리스트 바로 아래 고정 페이지네이션 */}
-    <div className="mt-auto">
+      <div className="mt-4">
       <Pagination
         page={page}
         totalPages={totalPages}
